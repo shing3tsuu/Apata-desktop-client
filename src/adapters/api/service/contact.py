f75c3134bfdfeb6b59eacd49f6c44967d88080ed
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-10-11 20:41:04 +0400

    Update contact.py

--- src/adapters/api/service/contact.py
+++ src/adapters/api/service/contact.py
@@ -4,13 +4,21 @@ import asyncio
 from datetime import datetime
 
 from ..dao.contact import ContactHTTPDAO
+from ..dao.auth import AuthHTTPDAO
 
 class ContactHTTPService:
-    def __init__(self, contact_dao: ContactHTTPDAO):
+    def __init__(self, contact_dao: ContactHTTPDAO, auth_dao: AuthHTTPDAO):
         self._contact_dao = contact_dao
+        self._auth_dao = auth_dao
 
     async def search_users(self, username: str, token: str) -> list[dict[str, Any]]:
-        return await self._contact_dao.search_users(username, token)
+        return await self._contact_dao.search_users(username=username, token=token)
+
+    async def contact_data_synchronization(self, users_ids: list[int], token: str) -> list[dict[str, Any]]:
+        if not users_ids:
+            return []
+        result = await self._contact_dao.get_users_data(users_ids=users_ids, token=token)
+        return result
 
     async def send_contact_request(self, sender_id: int, receiver_id: int, token: str) -> dict[str, Any]:
         return await self._contact_dao.send_contact_request(sender_id, receiver_id, token)
